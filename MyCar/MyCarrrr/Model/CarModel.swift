c43f1f60b14b0b9a9cbeb4cdde470fa1d6a83efe
SER0021 <ser00021@yandex.ru>
2024-05-12 20:17:13 +0300

    change userdefaults to filemanager

--- MyCar/MyCarrrr/Model/CarModel.swift
+++ MyCar/MyCarrrr/Model/CarModel.swift
@@ -1,5 +1,37 @@
-import Foundation
+import UIKit
 
-struct CellContent {
-    let manufacturer: String
-}
+
+//func getDocumentsDirectory() -> URL {
+//    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//    return paths[0]
+//}
+//
+//func saveCars2(_ cars: [CarViewModel]) {
+//    let encoder = JSONEncoder()
+//    do {
+//        let data = try encoder.encode(cars)
+//        let fileURL = getDocumentsDirectory().appendingPathComponent("cars.json")
+//        try data.write(to: fileURL)
+//        print("Данные успешно сохранены")
+//    } catch {
+//        print("Ошибка при сохранении данных: \(error.localizedDescription)")
+//    }
+//}
+//
+//func loadCars2() -> [CarViewModel] {
+//    let fileURL = getDocumentsDirectory().appendingPathComponent("cars.json")
+//    if FileManager.default.fileExists(atPath: fileURL.path) {
+//        do {
+//            let data = try Data(contentsOf: fileURL)
+//            let decoder = JSONDecoder()
+//            let loadedCars = try decoder.decode([CarViewModel].self, from: data)
+//            print("Загружено \(loadedCars.count) автомобилей")
+//            return loadedCars
+//        } catch {
+//            print("Ошибка при загрузке данных: \(error.localizedDescription)")
+//        }
+//    } else {
+//        print("Файл данных не найден")
+//    }
+//    return []
+//}
